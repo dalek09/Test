77bd8acd8084459c0f05c55cd0e2c2344f0cc491
Derek <derek.witchlow@bleepcomputing.com>
2017-12-22 12:19:14 +0000

    Added some code to test writing a file

--- app/src/main/java/com/example/derek/test/MainActivity.java
+++ app/src/main/java/com/example/derek/test/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.derek.test;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
+import java.io.IOException;
+
+import static android.os.Environment.getExternalStorageDirectory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void readFile(View v) {
+        String myFolderName =  getExternalStorageDirectory() + "/Derek/";
+        String myFileName = "Bleep.CSV";
+
+        // if the folder doesn't exist, create it
+        if(!dir_exists(myFolderName)) {
+            File dir = new File(myFolderName);
+            dir.mkdirs();
+        }
+
+        String fullName = myFolderName + myFileName;
+        File myFile= new File( fullName );
+        try {
+            myFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // return true if the specified directory exists
+    private boolean dir_exists(String dirPath) {
+    boolean ret = false;
+    File dir = new File(dirPath);
+    if(dir.exists() & dir.isDirectory()){
+        ret = true;
+    }
+    return ret;
     }
 }
