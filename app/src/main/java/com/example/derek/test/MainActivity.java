e480356a9ae3cb87de2a01498bf1a4b45879b13d
Derek <derek.witchlow@bleepcomputing.com>
2017-12-27 12:03:37 +0000

    Added progress box

--- app/src/main/java/com/example/derek/test/MainActivity.java
+++ app/src/main/java/com/example/derek/test/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.derek.test;
 
+import android.app.Dialog;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,6 +15,11 @@ import java.io.IOException;
 import static android.os.Environment.getExternalStorageDirectory;
 
 public class MainActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    Dialog progressBox;
+    int downloadedSize = 0;
+    int totalSize = 0;
+    TextView cur_val;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,22 +28,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getFile(View v) throws IOException {
-        //http://bleepcomputing.com/demo/TOLOrder-20171215-0010000003.CSV
+        String strURL = "http://bleepcomputing.com/demo/TOLOrder-20171215-0010000003.CSV";
     }
 
     public void readFile(View v) throws IOException {
-        String myFolderName =  getExternalStorageDirectory() + "/Derek/";
+        String myFolderName = getExternalStorageDirectory() + "/Derek/";
         String myFileName = "Bleep.CSV";
 
         // if the folder doesn't exist, create it
-        if(!dir_exists(myFolderName)) {
+        if (!dir_exists(myFolderName)) {
             File dir = new File(myFolderName);
-            if(!dir.mkdirs())
+            if (!dir.mkdirs())
                 throw new IOException("Cannot make directory: " + myFolderName);
         }
 
         String fullName = myFolderName + myFileName;
-        File myFile= new File( fullName );
+        File myFile = new File(fullName);
         try {
             myFile.createNewFile();
         } catch (IOException e) {
@@ -44,11 +53,29 @@ public class MainActivity extends AppCompatActivity {
 
     // return true if the specified directory exists
     private boolean dir_exists(String dirPath) {
-    boolean ret = false;
-    File dir = new File(dirPath);
-    if(dir.exists() & dir.isDirectory()){
-        ret = true;
+        boolean ret = false;
+        File dir = new File(dirPath);
+        if (dir.exists() & dir.isDirectory()) {
+            ret = true;
+        }
+        return ret;
     }
-    return ret;
+
+    void showProgress(String file_path) {
+        progressBox = new Dialog(MainActivity.this);
+        progressBox.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        progressBox.setContentView(R.layout.progress_dialog);
+        progressBox.setTitle("Download Progress");
+
+        TextView text = (TextView) progressBox.findViewById(R.id.tv1);
+        text.setText("Downloading file from ... " + file_path);
+        cur_val = (TextView) progressBox.findViewById(R.id.cur_pg_tv);
+        cur_val.setText("Starting download...");
+        progressBox.show();
+
+        progressBar = (ProgressBar) progressBox.findViewById(R.id.progress_bar);
+        progressBar.setProgress(0);
+        progressBar.setProgressDrawable(getResources().getDrawable(R.drawable.green_progress));
     }
+
 }
